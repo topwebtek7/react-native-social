cb8fcf1bf7b4d46c07e86679cbfef48da54ec733
Brian <brianwebtek@gmail.com>
2019-08-03 07:03:35 -0700

    Update post items with media content, add polls for type is poll

--- client/src/Feed.js
+++ client/src/Feed.js
@@ -7,7 +7,7 @@ import {
   Text,
   ListItem
 } from "react-native-ui-kitten";
-import { View } from "react-native";
+import { View, Image } from "react-native";
 import { SafeAreaView } from "react-navigation";
 import Icon from "react-native-vector-icons/Feather";
 import Axios from "axios";
@@ -17,12 +17,20 @@ const FeedEntry = ({ item }) => (
     <View
       style={{ margin: 12, flexDirection: "row", alignItems: "flex-start" }}
     >
-      <Avatar style={{ marginTop: 4 }} source={{ uri: item.author.avatar }} />
+      <Avatar style={{ marginTop: 4 }} source={{ uri: item.author.avatar }} style={{width: 40, height: 40}} />
       <View style={{ marginLeft: 12, flex: 1 }}>
+      { item.media && item.media.length > 0 && <Image source={{uri: item.media[0].uri}} style={{width: '100%', height: 100}} />}
         <Text category="s1">{item.author.name}</Text>
         <Text style={{ flex: 1 }} category="p2">
           {item.body}
         </Text>
+        { item.type === "poll" && 
+          <List
+            renderItem={renderPoll}
+            data={item.options}
+            keyExtractor={item => item.id}
+          />
+        }
         <View
           style={{
             marginTop: 12,
@@ -39,12 +47,24 @@ const FeedEntry = ({ item }) => (
   </ListItem>
 );
 
+const renderPoll = option => {
+  console.log(option);
+  return (
+    <ListItem
+      title={option.item.text}
+      description={`Responses: ${option.item.responses}`}
+      titleStyle={{ color: '#000' }}
+      descriptionStyle={{ color: '#000' }}
+      style={{ color: '#000' }}
+    />
+  )
+};
+
 const usePosts = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     Axios.get("http://localhost:7070/").then(({ data }) => {
-      console.log("posts", data);
       setPosts(data || []);
     });
   }, []);
